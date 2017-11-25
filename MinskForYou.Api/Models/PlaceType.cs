aa687a1a408bb107a65c7ec6fb3cf8781c9855d1
DESKTOP-23SINCF\Katya <katherine.ilyashevich@gmail.com>
2017-11-25 14:26:42 +0300

    PlaceTypes added.

---
+++ MinskForYou.Api/Models/PlaceType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinskForYou.Api.Models {
+	public enum PlaceType {
+		Other,
+		Museum,
+		Theatre,
+		Library,
+		Monument,
+		Park,
+		Entertainments,
+		СafesAndRestaurants,
+		BarsAndPubs
+	}
+}
