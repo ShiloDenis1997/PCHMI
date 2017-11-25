2036bebc1609436aedbfa6e7967d67f54156c9f3
DESKTOP-23SINCF\Katya <katherine.ilyashevich@gmail.com>
2017-11-25 19:51:08 +0300

    Android client with MasterDetailsPage added. But this Saturday could be so good:( Thank you, Xamarin, for troubles!!!

---
+++ AndroidClient/AndroidClient/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AndroidClient
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			if (Device.RuntimePlatform == Device.Android)
+				MainPage = new AndroidClient.Droid.Menu.Menu();
+			else
+				MainPage = new AndroidClient.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
