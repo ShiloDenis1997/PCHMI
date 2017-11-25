d2670372377d89f841af19b67df71fc5a21bc7ce
DESKTOP-23SINCF\Katya <katherine.ilyashevich@gmail.com>
2017-11-25 16:28:14 +0300

    Empty services for users and comments was added.

--- MinskForYou.Api/LogicModule.cs
+++ MinskForYou.Api/LogicModule.cs
@@ -6,10 +6,12 @@ using MinskForYou.Api.Interfaces.Services;
 namespace MinskForYou.Api {
 
 	public class LogicModule : Module {
+
 		protected override void Load(ContainerBuilder builder) {
-
+			builder.RegisterType<LoginService>().As<ILoginService>().SingleInstance();
 			builder.RegisterType<PlaceService>().As<IPlaceService>().SingleInstance();
-
+			builder.RegisterType<CommentService>().As<ICommentService>().SingleInstance();
 		}
 	}
+
 }
---
+++ MinskForYou.Api/Services/LoginService.cs
@@ -0,0 +1,10 @@
+﻿using MinskForYou.Api.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinskForYou.Api.Services {
+	internal class LoginService : ILoginService {
+	}
+}
